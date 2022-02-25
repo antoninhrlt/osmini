93224d5fa8232d7d1e5ed0e64a230f678eb5ef95
antoninhrlt <antoninhrlt1@gmail.com>
2022-02-25 15:16:39 +0100

    Update build files

--- build/build.mk
+++ build/build.mk
@@ -3,25 +3,25 @@
 # Copyright (c) Antonin Hérault
 
 ASM = nasm
+LINKER = ld
 
 OUT_DIR = build/out
-EXT = osmini
 
 IMG = $(OUT_DIR)/osmini.img
-BOOT = $(OUT_DIR)/bootloader.$(EXT)
-CORE = $(OUT_DIR)/core.$(EXT)
-
-BOOT_SRC = $(wildcard src/boot/*.asm)
+BOOT = $(OUT_DIR)/bootloader
 
 build : $(IMG)
+
+# Private function to simplify the release creation
+# Should not be used by another one than the repository's owner
+_release : $(IMG)
+	cp $< bin/
 
 run : $(IMG)
 	qemu-system-x86_64 -boot a -drive file=$<,format=raw,index=0,media=disk
 
-$(IMG) : $(BOOT) $(CORE)
+$(IMG) : $(BOOT)
 	cat $^ /dev/zero | dd of=$@ bs=512 count=2880 
 
-$(BOOT) : $(BOOT_SRC)
-	$(ASM) -f bin -o $@ $^
-
-$(CORE) : 
+$(BOOT) : src/boot/main.asm
+	$(ASM) -f bin -I src/boot/ -o $@ $^
