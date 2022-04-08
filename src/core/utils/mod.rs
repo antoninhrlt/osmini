979ebe58c86aa8a72a416dcda1df5449eba2b48d
antoninhrlt <antoninhrlt1@gmail.com>
2022-04-08 23:47:15 +0200

    Remove utils module

--- src/core/utils/mod.rs
+++
@@ -1,6 +0,0 @@
-// This file is part of "osmini"
-// Under the MIT License
-// Copyright (c) Antonin Hérault
-
-pub mod buffer;
-pub mod point;
--- src/core/utils/point.rs
+++
@@ -1,8 +0,0 @@
-// This file is part of "osmini"
-// Under the MIT License
-// Copyright (c) Antonin Hérault
-
-pub struct Point {
-    pub x: usize,
-    pub y: usize,
-}
