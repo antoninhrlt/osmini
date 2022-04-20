f9c9449ce58a98e90e5a091286bbe0835f6dac65
antoninhrlt <antoninhrlt1@gmail.com>
2022-04-20 18:24:31 +0200

    Add panic handler for bootloader

--- bootloader/src/lib.rs
+++ bootloader/src/lib.rs
@@ -2,6 +2,15 @@
 // Under the MIT License
 // Copyright (c) 2022 Antonin Hérault
 
+#![no_std]
+
 extern "C" {
     fn _bootloader() -> !;
+}
+
+// Panic handler need when `#![no_std]`
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
 }
